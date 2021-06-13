73950f9b28883f1248b4090a6c8438cb2995eba1
freq31 <70052807+freq31@users.noreply.github.com>
2021-06-13 18:15:09 +0530

    adding views

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,6 @@
 module.exports.posts=function(req,res){
-    return res.end('<h1>user posts</h1>');
+    //return res.end('<h1>user posts</h1>');
+    return res.render('posts',{
+        title:"users posts"
+    });
 }
