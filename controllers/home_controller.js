8ba84cce5d224ae5da33f9b3691febcfc599ec22
freq31 <70052807+freq31@users.noreply.github.com>
2021-07-01 20:05:48 +0530

    displaying comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,19 @@
 const Post=require('../models/post');
 module.exports.home=function(req,res){
-    Post.find({}).populate('user').exec(function(err,post){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,post){
         if(err){
-            console.log('error in fetching posts from db');
+            console.log('error in fetching posts from db',err);
             return;
         }
+        console.log(post);
         return res.render('home',{
             title:"home",
             posts:post,
